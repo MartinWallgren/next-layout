5f98680369eb7656c75eaac4f2155ddae042567e
Martin Wallgren <martinwa@axis.com>
2020-08-11 12:28:27 +0200

    Use group number as identifier
    It was not possible to switch between to layouts with the same symbol
    (for example "se" and "se -variant us").
    
    This change use the group number as identifier instead.

--- next_layout/cli.py
+++ next_layout/cli.py
@@ -4,18 +4,21 @@ import click
 from xkbgroup import XKeyboard
 
 
-def next_layout():
+def next_layout(xkb):
     """Switch to the next keyboard layout found by xkbgroup
     """
-    xkb = XKeyboard()
     if xkb.groups_count < 2:
         return  # nothing to do
-    symbol_index = xkb.groups_symbols.index(xkb.group_symbol)
-    xkb.group_symbol = xkb.groups_symbols[(symbol_index + 1) % len(
-        xkb.groups_symbols)]
+    number = xkb.group_num
+    new_number = (number + 1) % len(xkb.groups_data)
+    xkb.group_num = new_number
+
+
 
 
 @click.command()
 def main():
     """Switch to the next keyboard layout"""
-    next_layout()
+    xkb = XKeyboard()
+    next_layout(xkb)
+    print(xkb.group_name)
